73b189209a51d6a72767a08d31048c82a7055e6d
danjessy <danjessy927@gmail.com>
2026-04-19 01:11:29 +0200

    Add files via upload

---
+++ vinyle.js
@@ -0,0 +1,34 @@
+// Récupérer l'id dans l'URL
+const params = new URLSearchParams(window.location.search);
+const id = params.get("id");
+
+// Trouver le bon vinyle
+const vinyle = vinyles.find(v => v.id == id);
+
+// Sécurité minimale
+if (!vinyle) {
+  document.body.innerHTML = "<h1>Vinyle introuvable</h1>";
+}
+
+// Affichage
+document.getElementById("title").textContent = vinyle.title;
+document.getElementById("cover").src = vinyle.cover;
+
+document.getElementById("artist").textContent =
+  "Artiste : " + (Array.isArray(vinyle.artist) ? vinyle.artist.join(", ") : vinyle.artist);
+
+document.getElementById("year").textContent =
+  "Année : " + vinyle.year;
+
+document.getElementById("genre").textContent =
+  "Genre : " + (Array.isArray(vinyle.genre) ? vinyle.genre.join(", ") : vinyle.genre);
+
+
+
+const trackList = document.getElementById("tracks");
+
+vinyle.tracks.forEach(track => {
+  const li = document.createElement("li");
+  li.textContent = `${track.title} (${track.duration})`;
+  trackList.appendChild(li);
+});
